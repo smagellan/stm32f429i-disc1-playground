b10bd9c14a99979b8d538e45a86fdbece68035c6
vladimir <smagellan@gmail.com>
2019-05-08 23:26:46 +0300

    added red-rocket-computing backtrace demo

---
+++ redrocket-backtrace/rr-btrace/bin/clock.d
@@ -0,0 +1,27 @@
+bin/clock.o: clock.c ../../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../../libopencm3/include/libopencm3/cm3/common.h \
+ /opt/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h \
+ /opt/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_stdint.h \
+ /opt/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdbool.h \
+ ../../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../../libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ ../../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../../libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ ../../libopencm3/include/libopencm3/stm32/pwr.h \
+ ../../libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ ../../libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ ../../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../../libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ ../../libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ ../../libopencm3/include/libopencm3/cm3/nvic.h \
+ ../../libopencm3/include/libopencm3/dispatch/nvic.h \
+ ../../libopencm3/include/libopencm3/stm32/f4/nvic.h \
+ ../../libopencm3/include/libopencm3/cm3/systick.h clock.h
